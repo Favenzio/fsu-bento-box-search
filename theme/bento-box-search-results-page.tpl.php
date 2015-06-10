ae172778b22ac5b76b29228ea3cf819ada06e019
fcalvo@fsu.edu <>
2015-06-10 13:20:48 -0400

    Added function that retrieves all nodes of type Search Source.

--- theme/bento-box-search-results-page.tpl.php
+++ theme/bento-box-search-results-page.tpl.php
@@ -9,3 +9,8 @@
 
 <p>Your search for <strong><?php print $search_input; ?></strong> 
 returned the following results:</p>
+
+<?php foreach($search_sources as $search_source): ?>
+<h4>From <?php print $search_source->title; ?></h4>
+<div id="<?php print $search_source->nid; ?>">results</div>
+<?php endforeach; ?>
