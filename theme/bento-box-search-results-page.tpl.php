c06505e8ad3b6b64b93c1065635954786cb818bf
fcalvo@fsu.edu <>
2015-06-12 17:39:52 -0400

    Refactored HTML parsing functions to new file html_parser.inc

--- theme/bento-box-search-results-page.tpl.php
+++ theme/bento-box-search-results-page.tpl.php
@@ -11,6 +11,8 @@
 returned the following results:</p>
 
 <?php foreach($search_sources as $search_source): ?>
-<h4>From <?php print $search_source->title; ?></h4>
-<div id="<?php print $search_source->nid; ?>"></div>
+
+<div id="<?php print $search_source->nid; ?>" class="bento-search-system-container">
+  <h4>From the <?php print $search_source->title; ?></h4>
+</div>
 <?php endforeach; ?>
